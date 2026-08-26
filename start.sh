f75f8d6c191bc3627ff1414be8f52dcb46114c5d
machinoolio <mastersvanity@gmail.com>
2026-08-26 13:15:34 -0700

    fix(build): install submodules

--- start.sh
+++ start.sh
@@ -93,8 +93,8 @@ function custom_pre_hooks(){
   podman-chroot 'pacman -S --noconfirm --needed ninja meson >/dev/null'
   
   # clone bootc-installer and install it
-  podman-chroot 'su builder -c "git clone -b v3.0.16 --depth 1 https://github.com/projectbluefin/bootc-installer /buildhome/bootc-installer" >/dev/null'
-  podman-chroot 'su builder -c "cd /buildhome/bootc-installer && meson setup build && ninja -C build && sudo ninja -C build install"'
+  podman-chroot 'su builder -c "git clone --recurse-submodules -b v3.0.16 --depth 1 https://github.com/projectbluefin/bootc-installer /buildhome/bootc-installer" >/dev/null'
+  podman-chroot 'su builder -c "cd /buildhome/bootc-installer && meson setup build && ninja -C build && sudo ninja -C build install" >/dev/null'
   podman-chroot 'userdel builder && rm -rf /buildhome /etc/sudoers.d'
 
   # remove build pkgs
