041238f0009762da7077cfa09eaca40bcafd9c99
machinoolio <mastersvanity@gmail.com>
2026-09-02 07:31:18 -0700

    fix(build): put &&

--- start.sh
+++ start.sh
@@ -81,7 +81,7 @@ podman-chroot 'runuser -u builder -- bash -c "cd /buildhome/bootc-installer && g
 
 # clone fisherman and install it to /usr/local/bin/fisherman
 podman-chroot 'runuser -u builder -- bash -c "git clone --quiet https://github.com/projectbluefin/fisherman /buildhome/fisherman && cd /buildhome/fisherman && git switch --detach 35c8f6f"'
-podman-chroot 'runuser -u builder -- bash -c "cd /buildhome/fisherman/fisherman && git apply /app/patches/fisherman-var-tmp-fix.patch GOCACHE=/buildhome/gocache GOPATH=/buildhome/gopath GOPROXY=off go build -o /buildhome/fisherman/fisherman-bin ./cmd/fisherman && sudo install -Dm755 /buildhome/fisherman/fisherman-bin /usr/local/bin/fisherman"'
+podman-chroot 'runuser -u builder -- bash -c "cd /buildhome/fisherman/fisherman && git apply /app/patches/fisherman-var-tmp-fix.patch && GOCACHE=/buildhome/gocache GOPATH=/buildhome/gopath GOPROXY=off go build -o /buildhome/fisherman/fisherman-bin ./cmd/fisherman && sudo install -Dm755 /buildhome/fisherman/fisherman-bin /usr/local/bin/fisherman"'
 
 # cleanup
 podman-chroot 'userdel builder && rm -rf /buildhome /etc/sudoers.d'
