d13cab08594a334409d96d82422546677cb05009
machinoolio <mastersvanity@gmail.com>
2026-09-08 06:19:37 -0700

    fix(scripts): rm outside of podman-chroot

--- start.sh
+++ start.sh
@@ -81,8 +81,9 @@ podman-chroot 'runuser -u builder -- bash -c "git clone --quiet --recurse-submod
 podman-chroot 'runuser -u builder -- bash -c "cd /buildhome/bootc-installer && git apply /app/patches/force-sudo.patch && meson setup build --prefix=/usr --reconfigure && ninja -C build && sudo ninja -C build install"'
 
 # clone fisherman and install it to /usr/local/bin/fisherman, replaces /usr/bin/fisherman installed by bootc-installer
-podman-chroot 'runuser -u builder -- bash -c "rm -f /usr/bin/fisherman && git clone --quiet https://github.com/projectbluefin/fisherman /buildhome/fisherman && cd /buildhome/fisherman"'
+podman-chroot 'runuser -u builder -- bash -c "git clone --quiet https://github.com/projectbluefin/fisherman /buildhome/fisherman && cd /buildhome/fisherman"'
 podman-chroot 'runuser -u builder -- bash -c "cd /buildhome/fisherman/fisherman && GOCACHE=/buildhome/gocache GOPATH=/buildhome/gopath GOPROXY=off go build -o /buildhome/fisherman/fisherman-bin ./cmd/fisherman && sudo install -Dm755 /buildhome/fisherman/fisherman-bin /usr/local/bin/fisherman"'
+sudo rm -f $SQUASHFS_CTR_IMAGE_MOUNTPOINT/usr/bin/fisherman
 
 # cleanup
 podman-chroot 'userdel builder && rm -rf /buildhome /etc/sudoers.d'
