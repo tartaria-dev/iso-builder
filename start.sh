f216d63d6b966ca228a3ae2fd3b069c175a5f0d5
machinoolio <mastersvanity@gmail.com>
2026-09-09 20:37:11 -0700

    fix(installer): just change what groups are passed instead

--- start.sh
+++ start.sh
@@ -78,10 +78,10 @@ sudo mkdir -p $SQUASHFS_CTR_IMAGE_MOUNTPOINT/usr/local/bin
 
 # clone bootc-installer and install it
 podman-chroot 'runuser -u builder -- bash -c "git clone --quiet --recurse-submodules -b latest-dev --depth 1 https://github.com/tuna-os/bootc-installer /buildhome/bootc-installer"'
-podman-chroot 'runuser -u builder -- bash -c "cd /buildhome/bootc-installer && git apply /app/patches/force-sudo-as-priv-esc.patch && meson setup build --prefix=/usr --reconfigure && ninja -C build && sudo ninja -C build install"'
+podman-chroot 'runuser -u builder -- bash -c "cd /buildhome/bootc-installer && git apply /app/patches/{force-sudo-as-priv-esc,change-default-groups}.patch && meson setup build --prefix=/usr --reconfigure && ninja -C build && sudo ninja -C build install"'
 
 # clone fisherman and install it to /usr/local/bin/fisherman, replaces /usr/bin/fisherman installed by bootc-installer
-podman-chroot 'runuser -u builder -- bash -c "git clone --quiet https://github.com/tuna-os/fisherman /buildhome/fisherman && cd /buildhome/fisherman && git apply /app/patches/{var-tmp-no-tmpfs,no-pull-on-live-iso,filter-missing-groups}.patch"'
+podman-chroot 'runuser -u builder -- bash -c "git clone --quiet https://github.com/tuna-os/fisherman /buildhome/fisherman && cd /buildhome/fisherman && git apply /app/patches/{var-tmp-no-tmpfs,no-pull-on-live-iso}.patch"'
 podman-chroot 'runuser -u builder -- bash -c "cd /buildhome/fisherman/fisherman && GOCACHE=/buildhome/gocache GOPATH=/buildhome/gopath GOPROXY=off go build -o /buildhome/fisherman/fisherman-bin ./cmd/fisherman && sudo install -Dm755 /buildhome/fisherman/fisherman-bin /usr/local/bin/fisherman"'
 sudo rm -f $SQUASHFS_CTR_IMAGE_MOUNTPOINT/usr/bin/fisherman
 
